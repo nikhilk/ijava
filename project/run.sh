6d62135272ca966e2a0943d0d74276b02d426c1c
Nikhil Kothari <nikhilko@google.com>
2014-09-20 01:15:43 -0700

    Misc repo renames

--- project/run.sh
+++ project/run.sh
@@ -3,6 +3,6 @@
 mkdir -p /tmp/notebooks
 
 ipython notebook --ip="0.0.0.0" --port=8080 --notebook-dir=/tmp/notebooks \
-  --config=../ipython/config.py \
+  --config=../profile/config.py \
   --matplotlib=inline --no-mathjax
 
