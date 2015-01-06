44b1091b0e491442da290b651f69f8b0bbe97b41
Nikhil Kothari <nikhilko@google.com>
2015-01-05 16:46:12 -0800

    Fix pom.xml files to keep eclipse happy

--- tools/build.sh
+++ tools/build.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-mkdir -p ../build/outputs
-mvn clean package -DskipTests -l ../build/outputs/maven.log -f ../sources
+mkdir -p ../build
+mvn clean package -DskipTests -l ../build/maven.log -f ../sources
 
 if [ "$?" = "0" ]; then
-  cp ../build/outputs/api/api-0.1.jar ../build/ijavart.jar
-  cp ../build/outputs/extensions/charting/ijavaext-charting-0.1.jar ../build/ijavaext-charting.jar
+  cp ../sources/api/target/api-0.1.jar ../build/ijavart.jar
+  cp ../sources/extensions/charting/target/ijavaext-charting-0.1.jar ../build/ijavaext-charting.jar
 
-  cat ../sources/stub ../build/outputs/core/core-0.1.jar > ../build/ijava
+  cat ../sources/stub ../sources/core/target/core-0.1.jar > ../build/ijava
   chmod +x ../build/ijava
   ls -l ../build
 else
-  cat ../build/outputs/maven.log
+  cat ../build/maven.log
 fi
-
